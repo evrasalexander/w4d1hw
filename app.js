3c766d46d272557d8454d03e11e4052db14a773e
Alexander Evras <evrasalexander@gmail.com>
2022-01-24 19:55:22 -0500

    hw

---
+++ app.js
@@ -0,0 +1,25 @@
+const ourArray = [];
+
+for (let i = 1; i < 11; i++) {
+  ourArray.push(i);
+}
+
+const ourArray = [];
+let i = 1;
+
+while (i < 11) {
+  ourArray.push(i);
+  i++;
+}
+
+const ourArray = [];
+
+for (let i = 11; i < 23; i += 2) {
+  ourArray.push(i);
+}
+
+const ourArray = [];
+
+for (let i = 15; i > 0; i -= 2) {
+  ourArray.push(i);
+}
